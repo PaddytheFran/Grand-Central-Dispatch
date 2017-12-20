429260e38f9162613e160cb426c67e08223fd49f
Patrick Flanagan <patrickflanagan@patricks-imac.local>
2017-12-20 14:41:52 -0600

    Import swiftyjson and initial message parse.

--- HackingWithSwift - Project7/ViewController.swift
+++ HackingWithSwift - Project7/ViewController.swift
@@ -8,12 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+	var petitions = [[String: String]]()
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+		
+		let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+		
+		if let url = URL(string : urlString) {
+			
+			if let data = try? String(contentsOf: url) {
+				
+				let json = JSON(parseJSON: data)
+				if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+				
+				// we're okay to parse!
+				}
+			}
+		}
+	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
